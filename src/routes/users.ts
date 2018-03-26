63c80e7cfd327b50866bf76417724d83b5acad8d
Tomas Voslar <tomas.voslar@iflix.com>
2018-03-27 01:01:40 +0200

    Added interface for user, select attributes from database.

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,21 +1,30 @@
-import { Path, GET, PathParam } from 'typescript-rest'
+import { Path, GET, PathParam, Errors } from 'typescript-rest'
 import { Inject } from 'typescript-ioc'
 import Database from '../services/database'
 import User from '../models/user.model'
+import { IUser } from '../types/models.d'
 
 @Path('/api/users')
 export default class UsersController {
   @Inject
   private database: Database
 
+  private readonly ATTRIBUTES = ['id', 'firstname', 'lastname', 'username', 'email', 'createdAt', 'updatedAt']
+
   @GET
-  async getUsers(): Promise<any[]> {
-    return await this.database.models.User.findAll()
+  async getUsers(): Promise<IUser[]> {
+    return await this.database.models.User.findAll({
+      attributes: this.ATTRIBUTES
+    })
   }
 
   @GET
   @Path(':id')
-  async getUser(@PathParam('id') userId: number): Promise<any> {
-    return await this.database.models.User.findById(userId)
+  async getUser(@PathParam('id') userId: number): Promise<IUser> {
+    try {
+      return await this.database.models.User.findById(userId, { attributes: this.ATTRIBUTES })
+    } catch (e) {
+      throw new Errors.NotFoundError('User not found.')
+    }
   }
 }
---
+++ src/types/models.d.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+  id: string,
+  email: string,
+  firstname: string,
+  lastname: string,
+  username: string,
+  createdAt: Date,
+  updatedAt: Date
+}
