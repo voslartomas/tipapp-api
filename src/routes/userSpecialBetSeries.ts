6cc08f375f654778019232701666dc2f05087bb5
Tomas Voslar <tomas.voslar@iflix.com>
2018-04-09 23:51:35 +0200

    Updated user betting api.

--- src/routes/userSpecialBetSeries.ts
+++ src/routes/userSpecialBetSeries.ts
@@ -1,18 +1,28 @@
-import { Path, GET, POST, PUT, DELETE, PathParam, Errors } from 'typescript-rest'
+import { Path, GET, POST, PUT, DELETE, PathParam, Errors, Context, ServiceContext } from 'typescript-rest'
 import { Inject } from 'typescript-ioc'
 import Database from '../services/database'
 import UserSpecialBetSerie from '../models/userSpecialBetSerie.model'
 import { IUserSpecialBetSerie } from '../types/models.d'
 
-@Path('/api/user-special-bet-series')
+@Path('/api/leagues/:leagueId/user/bets/series')
 export default class UserSpecialBetSeriesController {
+  @Context
+  context: ServiceContext
 
   @Inject
   private database: Database
 
+  @PathParam('leagueId')
+  leagueId: number
+
   @GET
   async getUserSpecialBetSeries(): Promise<IUserSpecialBetSerie[]> {
-    return await this.database.models.UserSpecialBetSerie.findAll({})
+    const leagueUser = await this.database.models.LeagueUser.findOne({where: { userId: this.context.request['user'].id, leagueId: this.leagueId }})
+    if (!leagueUser) {
+      throw new Error('User not signed into league.')
+    }
+
+    return await this.database.models.UserSpecialBetSerie.findAll({ where: {leagueUserId: leagueUser.id} })
   }
 
   @GET
@@ -38,13 +48,27 @@ export default class UserSpecialBetSeriesController {
 
   @PUT
   @Path(':id')
-  async updateUserSpecialBetSerie(@PathParam('id') userSpecialBetSerieId: number, userSpecialBetSerie: any): Promise<IUserSpecialBetSerie> {
+  async updateUserSpecialBetSerie(@PathParam('id') userSpecialBetSerieId: number = 0, userSpecialBetSerie: any): Promise<IUserSpecialBetSerie> {
     const dbUserSpecialBetSerie = await this.database.models.UserSpecialBetSerie.findById(userSpecialBetSerieId)
 
     if (dbUserSpecialBetSerie) {
-      return await dbUserSpecialBetSerie.update(userSpecialBetSerie)
+      // check if userId match
+      const leagueUser = await this.database.models.LeagueUser.findOne({where: { userId: this.context.request['user'].id, leagueId: this.leagueId }})
+      if (leagueUser && leagueUser.id === dbUserSpecialBetSerie.leagueUserId) {
+        return await dbUserSpecialBetSerie.update(userSpecialBetSerie)
+      }
+
+      throw new Error('User not signed into this league.')
     } else {
-      return await this.database.models.UserSpecialBetSerie.create(userSpecialBetSerie)
+      const leagueUser = await this.database.models.LeagueUser.findOne({where: { userId: this.context.request['user'].id, leagueId: this.leagueId }})
+
+      if (leagueUser) {
+          userSpecialBetSerie.leagueUserId = leagueUser.id
+          userSpecialBetSerie.dateTime = new Date()
+          return await this.database.models.UserSpecialBetSerie.create(userSpecialBetSerie)
+      }
+
+      throw new Error('User not signed into this league.')
     }
   }
 
