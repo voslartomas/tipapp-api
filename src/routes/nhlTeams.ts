d07c0f845df76eab14f044ce337dc5a4f1c40fc1
Martin Rosenberg <nitram.rosenberg@gmail.com>
2018-04-19 20:57:40 +0200

    no two same teams or players

--- src/routes/nhlTeams.ts
+++ src/routes/nhlTeams.ts
@@ -14,13 +14,17 @@ export default class NHLTeamsController {
   @GET
   async createNHLTeams() {
     const response = await request.get('https://statsapi.web.nhl.com/api/v1/teams/')
-    response.body.teams.forEach(item => {
-      const team: any = {}
-      team.name = item.name
-      team.nickname = item.teamName
-      team.shortcut = item.abbreviation
-      team.sportId = 2
-      this.database.models.Team.create(team)
+    response.body.teams.forEach(async item => {
+      const dbTeam = await this.database.models.Team.findOne({where: {externalId: item.id}})
+      if (!dbTeam) {
+        const team: any = {}
+        team.name = item.name
+        team.nickname = item.teamName
+        team.shortcut = item.abbreviation
+        team.externalId = item.id
+        team.sportId = 2
+        this.database.models.Team.create(team)
+      }
     })
   }
 
@@ -30,12 +34,16 @@ export default class NHLTeamsController {
     const response = await request.get('https://statsapi.web.nhl.com/api/v1/teams/')
     response.body.teams.forEach(async item => {
       const playerRoster = await request.get(`https://statsapi.web.nhl.com/api/v1/teams/${item.id}/roster`)
-      playerRoster.body.roster.forEach(item => {
-        const player: any = {}
-        player.firstName = item.person.fullName
-        player.lastName = ''
-        player.isActive = true
-        this.database.models.Player.create(player)
+      playerRoster.body.roster.forEach(async item => {
+        const dbPlayer = await this.database.models.Player.findOne({where: {externalId: item.person.id}})
+        if (!dbPlayer) {
+          const player: any = {}
+          player.firstName = item.person.fullName
+          player.lastName = ''
+          player.isActive = true
+          player.externalId = item.person.id
+          this.database.models.Player.create(player)
+        }
       })
     })
   }
