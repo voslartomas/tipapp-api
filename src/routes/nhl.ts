550eb2f9959b5a369e59161f6c19934a60105227
Martin Rosenberg <nitram.rosenberg@gmail.com>
2018-04-22 13:32:47 +0200

    leaguePlayers added

--- src/routes/nhl.ts
+++ src/routes/nhl.ts
@@ -14,17 +14,17 @@ export default class NHLController {
   private database: Database
 
   @GET
-  @Path('/:leagueId/teams')
-  async createNHLTeams(@PathParam('leagueId') leagueId: number) {
-    const response = await request.get('https://statsapi.web.nhl.com/api/v1/teams/')
-    response.body.teams.forEach(async item => {
-      const dbTeam = await this.database.models.Team.findOne({where: {externalId: item.id}})
+  @Path('/:leagueId/')
+  async importNHL(@PathParam('leagueId') leagueId: number) {
+    const teamResponse = await request.get('https://statsapi.web.nhl.com/api/v1/teams/')
+    teamResponse.body.teams.forEach(async teamResponseItem => {
+      const dbTeam = await this.database.models.Team.findOne({where: {externalId: teamResponseItem.id}})
       if (!dbTeam) {
         const team: any = {}
-        team.name = item.name
-        team.nickname = item.teamName
-        team.shortcut = item.abbreviation
-        team.externalId = item.id
+        team.name = teamResponseItem.name
+        team.nickname = teamResponseItem.teamName
+        team.shortcut = teamResponseItem.abbreviation
+        team.externalId = teamResponseItem.id
         team.sportId = 2
         const dbTeam = await this.database.models.Team.create(team)
 
@@ -35,25 +35,27 @@ export default class NHLController {
         this.database.models.LeagueTeam.create(leagueTeam)
       }
     })
-  }
-
-  @GET
-  @Path('/players')
-  async createNHLPlayers() {
-    const response = await request.get('https://statsapi.web.nhl.com/api/v1/teams/')
-    response.body.teams.forEach(async item => {
-      const playerRoster = await request.get(`https://statsapi.web.nhl.com/api/v1/teams/${item.id}/roster`)
-      playerRoster.body.roster.forEach(async item => {
-        const dbPlayer = await this.database.models.Player.findOne({where: {externalId: item.person.id}})
+    const playerResponse = await request.get('https://statsapi.web.nhl.com/api/v1/teams/')
+    playerResponse.body.teams.forEach(async teamItem => {
+      const playerRoster = await request.get(`https://statsapi.web.nhl.com/api/v1/teams/${teamItem.id}/roster`)
+      playerRoster.body.roster.forEach(async playerRosterItem => {
+        const dbPlayer = await this.database.models.Player.findOne({where: {externalId: playerRosterItem.person.id}})
         if (!dbPlayer) {
           const player: any = {}
-          player.firstName = item.person.fullName
+          player.firstName = playerRosterItem.person.fullName
           player.lastName = ''
           player.isActive = true
-          player.externalId = item.person.id
-          this.database.models.Player.create(player)
+          player.externalId = playerRosterItem.person.id
+          const dbPlayer = await this.database.models.Player.create(player)
+
+          const dbTeam = await this.database.models.Team.findOne({ where: { externalId: teamItem.id } })
+          const dbLeagueTeam = await this.database.models.LeagueTeam.findOne({ where: { teamId: dbTeam.id, leagueId } })
 
           const leaguePlayer: any = {}
+          leaguePlayer.leagueTeamId = dbLeagueTeam.id
+          leaguePlayer.playerId = dbPlayer.id
+          leaguePlayer.bestScorer = false
+          this.database.models.LeaguePlayer.create(leaguePlayer)
         }
       })
     })
