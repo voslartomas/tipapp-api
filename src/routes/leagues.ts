f2761f28f509fc46dbec390808ff34d41b377d3e
Tomas Voslar <tomas.voslar@iflix.com>
2018-04-30 11:59:44 +0200

    Fix leaderboard sql. #4

--- src/routes/leagues.ts
+++ src/routes/leagues.ts
@@ -69,7 +69,7 @@ export default class LeaguesController {
     const users = await this.database.query(`SELECT "User"."firstName", "User"."lastName",
       (IFNULL((SELECT SUM(totalPoints) FROM "UserBet" WHERE "leagueUserId" = "LeagueUser"."id"), 0) +
       IFNULL((SELECT SUM(totalPoints) FROM "UserSpecialBetSerie" WHERE "leagueUserId" = "LeagueUser"."id"), 0) +
-      IFNULL((SELECT SUM(totalPoints) FROM "UserSpecialBetSingle" WHERE "leagueUserId" = "LeagueUser"."id"), 0)) AS totalPoints
+      IFNULL((SELECT SUM(totalPoints) FROM "UserSpecialBetSingle" WHERE "leagueUserId" = "LeagueUser"."id"), 0)) AS "totalPoints"
       FROM "LeagueUser"
       LEFT JOIN "User" ON "LeagueUser"."userId" = "User"."id" WHERE "leagueId" = ${leagueId}
       ORDER BY "totalPoints" DESC`, { type: this.database.QueryTypes.SELECT})
