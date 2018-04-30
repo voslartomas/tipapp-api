3d8f1d87a12308231761f82f5dd16b1702747135
Tomas Voslar <tomas.voslar@iflix.com>
2018-04-30 11:54:29 +0200

    Fix leaderboard sql.

--- src/routes/leagues.ts
+++ src/routes/leagues.ts
@@ -66,12 +66,12 @@ export default class LeaguesController {
   @GET
   @Path('/:leagueId/leaderboard')
   async getLeaderboard(@PathParam('leagueId') leagueId: number): Promise<any> {
-    const users = await this.database.query(`SELECT User.firstName, User.lastName,
-      (IFNULL((SELECT SUM(totalPoints) FROM UserBet WHERE leagueUserId = LeagueUser.id), 0) +
-      IFNULL((SELECT SUM(totalPoints) FROM UserSpecialBetSerie WHERE leagueUserId = LeagueUser.id), 0) +
-      IFNULL((SELECT SUM(totalPoints) FROM UserSpecialBetSingle WHERE leagueUserId = LeagueUser.id), 0)) AS totalPoints
+    const users = await this.database.query(`SELECT "User"."firstName", "User"."lastName",
+      (IFNULL((SELECT SUM(totalPoints) FROM UserBet WHERE leagueUserId = "LeagueUser"."id"), 0) +
+      IFNULL((SELECT SUM(totalPoints) FROM UserSpecialBetSerie WHERE leagueUserId = "LeagueUser"."id"), 0) +
+      IFNULL((SELECT SUM(totalPoints) FROM UserSpecialBetSingle WHERE leagueUserId = "LeagueUser"."id"), 0)) AS totalPoints
       FROM LeagueUser
-      LEFT JOIN User ON LeagueUser.userId = User.id WHERE leagueId = ${leagueId}
+      LEFT JOIN User ON "LeagueUser"."userId" = "User"."id" WHERE leagueId = ${leagueId}
       ORDER BY totalPoints DESC`, { type: this.database.QueryTypes.SELECT})
 
     return users
