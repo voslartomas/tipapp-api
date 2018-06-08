479674ca8947314abe8683e61d6233252547d4df
Roman <rotakt@gmail.com>
2018-06-08 14:02:21 +0200

    User data updated

--- src/models/fixtures/usersDefaultData.ts
+++ src/models/fixtures/usersDefaultData.ts
@@ -41,13 +41,102 @@ module.exports = {
             mobileNumber: '724724728',
             password: '12345'
         },
+        // od sestky
         {
-            firstName: 'Tomas',
-            lastName: 'Voslar',
+            firstName: 'Tomáš',
+            lastName: 'Voslař',
             username: 'tvoslar',
             email: 't.voslar@gmail.com',
             mobileNumber: '728888999',
-            password: 'test'
+            password: '123234'
+        },
+        {
+            firstName: 'Roman',
+            lastName: 'Talaš',
+            username: 'rtalas',
+            email: '',
+            mobileNumber: '',
+            password: '234345'
+        },
+        {
+            firstName: 'Radek',
+            lastName: 'Mikel',
+            username: 'rmikel',
+            email: '',
+            mobileNumber: '',
+            password: '345456'
+        },
+        {
+            firstName: 'Pavel Mikel',
+            lastName: 'pmikel',
+            username: '',
+            email: '',
+            mobileNumber: '',
+            password: '456567'
+        },
+        {
+            firstName: 'Martin',
+            lastName: 'Mikel',
+            username: 'mmikel',
+            email: '',
+            mobileNumber: '',
+            password: '567678'
+        },
+        {
+            firstName: 'Jaroslav',
+            lastName: 'Hamouz',
+            username: 'jhamouz',
+            email: '',
+            mobileNumber: '',
+            password: '678789'
+        },
+        {
+            firstName: 'Václav',
+            lastName: 'Dvorník',
+            username: 'vdvornik',
+            email: '',
+            mobileNumber: '',
+            password: '789890'
+        },
+        {
+            firstName: 'Tomáš',
+            lastName: 'Brebis',
+            username: 'tbrebis',
+            email: '',
+            mobileNumber: '',
+            password: '890901'
+        },
+        {
+            firstName: 'Filip',
+            lastName: 'Bartoš',
+            username: 'fbartos',
+            email: '',
+            mobileNumber: '',
+            password: '901012'
+        },
+        {
+            firstName: 'Jakub',
+            lastName: 'Mikulášek',
+            username: 'jmikulasek',
+            email: '',
+            mobileNumber: '',
+            password: '112123'
+        },
+        {
+            firstName: 'Lukáš',
+            lastName: 'Novák',
+            username: 'lnovak',
+            email: '',
+            mobileNumber: '',
+            password: '124345'
+        },
+        {
+            firstName: 'Jan',
+            lastName: 'Kořenek',
+            username: 'jkorenek',
+            email: '',
+            mobileNumber: '',
+            password: '245345'
         }
     ]
 
