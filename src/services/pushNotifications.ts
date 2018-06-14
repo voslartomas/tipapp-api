81dc3d4d1f4a71adb624ffbce2bfca04e9ac5a47
Tomas Voslar <tomas.voslar@iflix.com>
2018-06-14 03:26:28 +0200

    Fixes.

--- src/services/pushNotifications.ts
+++ src/services/pushNotifications.ts
@@ -1,10 +1,11 @@
 import Database from '../services/database'
 import Cache from '../services/cache'
-import { Inject } from 'typescript-ioc'
+import { Inject, Singleton } from 'typescript-ioc'
 import * as config from 'config'
 import * as moment from 'moment'
 import * as request from 'request'
 
+@Singleton
 export default class PushNotifications {
   @Inject
   private database: Database
@@ -22,7 +23,6 @@ export default class PushNotifications {
       const league = leagues[x]
       const users = await this.database.models.LeagueUser.findAll({ where: { leagueId: league.id }})
 
-      console.log(users.map(user => user.id))
       for (const i in users) {
         const leagueUser = users[i]
         // settings of user
@@ -34,24 +34,22 @@ export default class PushNotifications {
 
         const notifyBefore = moment(new Date()).add(user.notifyHours, 'hours').utc().format('YYYY-MM-DD HH:mm:ss')
         const now = moment(new Date()).utc().format('YYYY-MM-DD HH:mm:ss')
-        console.log('User:', leagueUser.id, user.id, league.id)
+
         const matches = await this.database.query(`
           SELECT "Match".* FROM "Match" LEFT JOIN "UserBet" ON "Match"."id" = "UserBet"."matchId" AND "UserBet"."leagueUserId" = ${leagueUser.id}
           WHERE "UserBet"."matchId" IS NULL
           AND "Match"."leagueId" = ${league.id} AND "Match"."dateTime" > '${now}' AND "Match"."dateTime" < '${notifyBefore}'
         `, { type: this.database.QueryTypes.SELECT})
 
-        console.log('count:', matches.length, matches)
         for (const m in matches) {
           const match = matches[m]
           const cacheKey = `${match.id}-${leagueUser.id}`
           const notified = await this.cache.get(cacheKey)
           if (!notified) {
             console.log('Notifying', user.firstName, user.pushId, match.id)
-            this.sendPushNotification(user.pushId)
+            this.sendPushNotification(user.pushId, 'tiapp') // Android
+            this.sendPushNotification(user.pushId, 'tipapp') // iOS
             await this.cache.set(cacheKey, { count: matches.length }, 24 * 60 * 60 * 60)
-          } else {
-            await this.cache.del(cacheKey)
           }
         }
 
@@ -62,9 +60,9 @@ export default class PushNotifications {
     // return await this.database.query(``, { type: this.database.QueryTypes.SELECT})
   }
 
-  sendPushNotification(deviceId) {
+  sendPushNotification(deviceId, app) {
     request({
-      url: 'https://api.appcenter.ms/v0.1/apps/t.voslar/tipapp/push/notifications',
+      url: `https://api.appcenter.ms/v0.1/apps/t.voslar/${app}/push/notifications`,
       method: 'POST',
       json: true,
       headers: {
@@ -83,17 +81,6 @@ export default class PushNotifications {
             }
         }
     })
-    console.log({
-        'notification_content' : {
-            'name' : 'Tipovani',
-            'title' : 'Je čas tipovat',
-            'body' : 'Za chvíli začíná zápas a ty ještě nemáš natipováno!'
-        },
-          'notification_target' : {
-            'type' : 'devices_target',
-            'devices' : [deviceId]
-          }
-      })
   }
 
   async sendPushNotifications() {
