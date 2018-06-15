fda67d312399d0bc6a3805d5f4e4547d69831771
Roman T <rotakt@gmail.com>
2018-06-15 18:17:30 +0200

    winner Evaluation fix (#26)

--- src/bets/evaluators/winner.ts
+++ src/bets/evaluators/winner.ts
@@ -3,11 +3,17 @@ import Match from '../../models/match.model'
 import LeagueSpecialBetSerie from '../../models/leagueSpecialBetSerie.model'
 import UserBet from '../../models/userBet.model'
 import UserSpecialBetSerie from '../../models/userSpecialBetSerie.model'
+import Exact from './exact'
 
 export default class Winner implements IEvaluator {
   type = 'winner'
 
   evaluateMatch(result: Match, tip: UserBet): boolean {
+
+    if (new Exact().evaluateMatch(result, tip)) {
+      return false
+    }
+
     return ((tip.homeScore - tip.awayScore > 0 && result.homeScore - result.awayScore > 0) ||
     (tip.homeScore - tip.awayScore < 0 && result.homeScore - result.awayScore < 0))
   }
